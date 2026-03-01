61bab8d7280fce8def8abe080f91e0b43f01aeae
Lilia Zhuravel <liliazuravel6@gmail.com>
2026-03-01 16:29:13 +0100

    Add custom exception

--- src/main/java/com/lilia/weather/exception/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.lilia.weather.exception;
-
-import java.time.LocalDateTime;
-
-public record ErrorResponse(
-        String message,
-        int status,
-        LocalDateTime timestamp
-) {}
